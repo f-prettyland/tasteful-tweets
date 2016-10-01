6ac6c900cbdc35117e8622a537aa2871b7931ef2
f-prettyland <f-prettyland@users.noreply.github.com>
2016-10-01 15:37:21 +0100

    Word changings

--- bot.py
+++ bot.py
@@ -8,25 +8,33 @@ import os
 import random
 from settings import api, __location__
 
-bad_words ={}
-nice_words ={}
+bad_nouns = {}
+nice_nouns = {}
+bad_adjectives = {}
+nice_adjectives = {}
 
-def happifier(to_find, replacee):
-  regex = re.compile('|'.join(r'(?:\s+|^)'+re.escape(x)+r'(?:\s+|$)' for x in to_find))
-  return regex.sub("",replacee)
+def happifier(replacee, replacers):
+  regex = re.compile('|'.join(r'(?:\s+|^)'+re.escape(x)+r'(?:\s+|$)'
+                      for x in bad_nouns))
+  randomChoice = random.randrange(len(replacers))
+  return regex.sub(" " + replacers[randomChoice] + " ", replacee)
 
 def status_replace():
   posts = api.get_user_timeline(screen_name = "HateToHearts")
-  try:
-    bad_words_file = open(os.path.join(__location__,'dict/bad.txt'),'r')
-    bad_words = [x.strip() for x in bad_words_file.readlines()]
-    bad_words_file.close()
+  for p in posts:
+    print(p['text'])
+    print(happifier(p['text'], nice_nouns))
 
-    for p in posts:
-      print(p['text'])
-      print(happifier(bad_words, p['text']))
-    return None
+def loadWords(loc):
+  try:
+    words_file = open(os.path.join(__location__,loc),'r')
+    words = [x.strip() for x in words_file.readlines()]
+    words_file.close()
+    return words
   except IOError:
-    return None
+    return {}
+
+bad_nouns = loadWords('dict/bad.txt')
+nice_nouns = loadWords('dict/good.txt')
 
 status_replace()
