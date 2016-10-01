b0e060cc626b16cdbd6e2deefdede74e574092e7
f-prettyland <f-prettyland@users.noreply.github.com>
2016-10-01 12:36:19 +0100

    Working posting, and scraping details, need to scrape statuses

---
+++ bot.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import sys
+import string
+import simplejson
+import os
+import random
+from settings import api
+
+# post(status=sys.argv[1]);
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def status_scrape():
+  # ids = "782139418517831680"
+  # stats = api.get_list_statuses(ids)
+  # followers = api.get_list_statuses(screen_name = "HateToHearts")
+  followers = api.get_list_statuses(slug="twitter", owner_screen_name="HateToHearts")
+
+  # users = api.lookup_user(user_id = ids)
+  for entry in followers:
+    print(entry)
+
+status_scrape()
+
+def randomTweet():
+  try:
+    tweetsFile = open(os.path.join(__location__,'tweets.txt'),'r')
+    tweetsList = tweetsFile.readlines()
+    tweetsFile.close()
+    randomChoice = random.randrange(len(tweetsList))
+    print (tweetsList[randomChoice]) #For debugging only
+    api.update_status(status=tweetsList[randomChoice])
+    return None
+  except IOError:
+    return None
+
+def post(tweet_s):
+  api.update_status(tweet_s)
+
+
+def scrape():
+  ids = "782139418517831680"
+  users = api.lookup_user(user_id = ids)
+
+  for entry in users:
+    r = {}
+    r['id'] = entry['id']
+    r['screen_name'] = entry['screen_name']
+    r['name'] = entry['name']
+    r['created_at'] = entry['created_at']
+    r['url'] = entry['url']
+    r['followers_count'] = entry['followers_count']
+    r['friends_count'] = entry['friends_count']
+    r['statuses_count'] = entry['statuses_count']
+    r['favourites_count'] = entry['favourites_count']
+    r['listed_count'] = entry['listed_count']
+    r['contributors_enabled'] = entry['contributors_enabled']
+    r['description'] = entry['description']
+    r['protected'] = entry['protected']
+    r['location'] = entry['location']
+    r['lang'] = entry['lang']
+    if 'url' in entry['entities']:
+        r['expanded_url'] = entry['entities']['url']['urls'][0]['expanded_url']
+    else:
+        r['expanded_url'] = ''
+    print(r)
